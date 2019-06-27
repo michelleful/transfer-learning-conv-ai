8649247b005517e710f36b1b9ba0e1d7831ee655
Michelle Fullwood <michelle.fullwood@gmail.com>
2019-06-27 18:17:58 -0400

    Added script to convert B's dataset to JSON format

---
+++ convert_to_json.py
@@ -0,0 +1,30 @@
+import json
+import random
+
+train = list()
+valid = list()
+
+with open("zipQA3.txt", "r") as f:
+    for i, line in enumerate(f):
+        if i == 0:
+            continue
+        zip_sep = line.find("|ZIP_SEP|")
+        prompt = line[:zip_sep]
+        response = line.strip()[zip_sep + len("|ZIP_SEP|"):]
+        if random.random() < 0.7:
+            train.append({
+                "prompt": prompt,
+                "response": response
+            })
+        else:
+            valid.append({
+                "prompt": prompt,
+                "response": response
+            })
+
+
+with open("zipQA3.json", "w") as g:
+    g.write(json.dumps({
+        "train": train,
+        "valid": valid
+    }))
